2027bcd554d5b636667b6ee6bae87328e1f1a330
Vytor Oliveira <156971837+Vytor-Oliveira@users.noreply.github.com>
2025-11-10 18:23:23 +0000

    Corrige  API na Vercel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
 const app = require('../backend/src/app');
 
-module.exports = (req, res) => {
-  req.url = req.url.replace(/^\/api(\/|$)/, '/') || '/';
-  return app(req, res);
-};
+module.exports = app;
